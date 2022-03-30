5a8dd48ec83e4285433baa56478576ac54a17462
ithud <83203188+ithud@users.noreply.github.com>
2022-03-30 10:38:37 +0800

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -4,7 +4,7 @@ module.exports = {
   publicPath: process.env.NODE_ENV === "production" ? "" : "/",
   productionSourceMap: false,
   devServer: {
-    host: "192.168.0.111",
+    host: "0.0.0.0",
     open: true,
   },
 };
